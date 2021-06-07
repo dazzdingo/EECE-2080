15d47d3c668c6f90654babef72e4d1b6590df0a4
Dazzdingoo <annguyen06092001@gmail.com>
2021-06-07 17:30:53 +0700

    modify for perfect

--- Lab 4/Task 3/Task3_1.cpp
+++ Lab 4/Task 3/Task3_1.cpp
@@ -5,25 +5,25 @@ using namespace std;
  
 int main()
 {
-   int value, choice;
-   while(1){
-      printf("\n\n***** MENU *****\n");
-      printf("1. Push\n2. Pop\n3. Top\n4. Display\n5. Exit");
-      printf("\nEnter your choice: ");
-      cin >> choice;
-      switch(choice){
-	 case 1: printf("Enter the value to be insert: ");
-		 cin >> value;
-		 push(value);
-		 break;
-	 case 3: cout << "\nTop element is " << top() << endl;
-     break;
-   case 2: pop();
-		 break;
-	 case 4: display();
-		 break;
-	 case 5: exit(0);
-	 default: printf("\nWrong selection! Please try again!");
-      }
-   }
+    int value, choice;
+    while(1){
+        printf("\n\n***** MENU *****\n");
+        printf("1. Push\n2. Pop\n3. Top\n4. Display\n5. Exit");
+        printf("\nEnter your choice: ");
+        cin >> choice;
+        switch(choice){
+	    case 1: printf("Enter the value to be insert: ");
+		    cin >> value;
+		    push(value);
+		    break;
+	    case 3: cout << "\nTop element is " << top() << endl;
+            break;
+        case 2: pop();
+		    break;
+	    case 4: display();
+		    break;
+	    case 5: exit(0);
+	    default: printf("\nWrong selection! Please try again!");
+        }
+    }
 }
